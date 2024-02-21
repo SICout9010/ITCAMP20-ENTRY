f88372d28217f06caeb3a3d2f9d655b8f2fa3a05
SICout9010 <fivrewgsd@gmail.com>
2024-02-21 23:36:56 +0700

    2024/2/21 Webhook feature update

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // 'Mr. Phuripat Just want to join ITCAMP20' index.js file
-// Manipulation 1
 let currentchatnum = 0;
 let heartsended = false;
 const nextMessage = () => {
     chatelement = document.querySelector(`[id$='${currentchatnum}']`);
 
-    // Manupulation 2
+    // Manupulation 1
     if (chatelement == null) {
         const endmessage = document.querySelector('.end-message');
         endmessage.style.visibility = 'visible';
         return
     }
-    // Manupulation 3
+    // Manupulation 2
     chatelement.style.visibility = 'visible'
     chatelement.style.animation = 'pull-chat 0.5s linear forwards'
     currentchatnum++;
@@ -21,18 +20,19 @@ const nextMessage = () => {
 const sendHeart = () => {
     if (heartsended) return;
 
+    const element = document.getElementById('messagetosend')
     const url = 'https://discord.com/api/webhooks/1109763599867584532/kTDWKZPA07T1c4K2wK7YrbcwYxcCDrdOtceYOXZ05qhYs-hf3xXE0sWhpxhSzku_Hs8e'
     const content = {
         content: "<@249427988997734401> New Feedback!",
         embeds: [{
-            title: "New Message!",
+            title: "Here a heart for you!",
             color: 3447003,
             footer: {
                 text: new Date().toUTCString()
             },
             fields: [{
                 name: "Feedback / Request",
-                value: "เอาหัวใจปายยยยยคร้าบบ ❤️❤️❤️"
+                value: element.value
             }]
         }]
       }
@@ -44,6 +44,7 @@ const sendHeart = () => {
                 'Content-Type': 'application/json'
             }
         }).then(() => {
+            // Manupulation 3
             document.querySelector('.send-love-container p').style.visibility = 'visible';
         })
     } catch (error) {
@@ -51,5 +52,6 @@ const sendHeart = () => {
         console.warn("Couldn't send Request to Webhook. Maybe Mr.Phuripat already delete it due to security issue?")
     }
 
+    element.value = '';
     heartsended = true;
 }
